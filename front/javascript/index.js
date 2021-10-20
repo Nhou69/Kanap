c6ba9bcce1a9d688e81b6c75ffc28d9ce2518a07
VANG Robert <robert.vang@hotmail.fr>
2021-10-20 22:01:32 +0200

    1er fecth sur l'index.js

--- front/javascript/index.js
+++ front/javascript/index.js
@@ -1,28 +1,28 @@
-const url = "http://localhost:3000/api/products"
+const url = "http://localhost:3000/api/products/"
 const itemsTag = document.getElementById("items")
 
 fetch(url)
 .then(reponse => reponse.json())
-.then(json => {
-    for(product of json) {
+.then(data => {
+    for(product of data) {
         console.log(product)
         itemsTag.innerHTML += `
         <a href="./product.html?id=${ product._id }">
             <article>
-              <img src="${ product.imageUrl }" alt=" ${ product.name }">
+              <img src="${ product.imageUrl }" alt="${ product.atlText}">
               <h3 class="productName">${ product.name }</h3>
-              <p class="productDescription"> ${ product.description }</p>
+              <p class="productDescription">${ product.description }</p>
             </article>
           </a>
         `
-
     }
 })
-.catch(erreur => console.error(erreur))
+.catch(error => console.error(error))
+
 
 /*fetch(url) 
-.then(function(response) {
-    return response.json()
+.then(function(reponse) {
+    return reponse.json()
 })
 .then(function(data) {
     return data;
--- front/javascript/product.js
+++ front/javascript/product.js
@@ -1,4 +1,23 @@
 // 1 . Récupérer l'identifiant du produit depuis l'url
+    //on récupère l'ensemble de l'url:
+const UrlId = new URL(window.location.href)
+console.log(UrlId)
+    //on récupère juste ce qu'il y a après le nom '?id'
+let getId = UrlId.searchParams.get("id")
+console.log(getId)
+
 // 2 . Construire une nouvelle url à partir de l'identifiant du produit
+const url = `http://localhost:3000/api/products/`
+console.log(url)
+    //concaténation des variables pour former une nouvelle url
+let newUrl = url + getId
+console.log (newUrl)
+
 // 3 . Appeler cette nouvelle url avec la méthode fetch
+fetch(newUrl)
+.then(reponse => reponse.json())
+.then((data) => console.log(data))
+.catch(error => console.error(error))
+
 // 4 . Remplacer les données statiques par le résultat du fetch
+const itemsImage = document.getElementsByClassName('item__img')
