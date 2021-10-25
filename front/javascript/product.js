06b497b6f5dc8da00c5f6802ef2dc3d7cc8c86e8
VANG Robert <robert.vang@hotmail.fr>
2021-10-25 12:02:35 +0200

    travaille sur product.js

--- front/javascript/product.js
+++ front/javascript/product.js
@@ -14,10 +14,38 @@ let newUrl = url + getId
 console.log (newUrl)
 
 // 3 . Appeler cette nouvelle url avec la méthode fetch
-fetch(newUrl)
-.then(reponse => reponse.json())
-.then((data) => console.log(data))
-.catch(error => console.error(error))
-
 // 4 . Remplacer les données statiques par le résultat du fetch 
 const itemsImage = document.getElementsByClassName('item__img')
+const itemsTitle = document.getElementById("title")
+const itemsDescription = document.getElementById("description")
+const itemsColors = document.getElementById("colors")
+
+fetch(newUrl)
+.then(reponse => reponse.json())
+.then(content => console.log(content))
+.then(content => {
+    let name = content.name
+    let description = content.description
+    let imageUrl = content.imageUrl
+    let altTxt = content.altTxt
+    let colors = content.colors
+    let price = content.price / 10
+    let id = content._id
+
+    itemsImage.innerHTML += `
+        <img src="${ imageUrl }" alt="${ atlText}">
+    `
+    itemsTitle.innerHTML += `
+        <h1 id="title">${ name }</h1>
+    `
+    itemsDescription.innerHTML += `
+        <p id="description">${ description }</p>
+    `
+    itemsColors.innerHTML += `
+        <select name="color-select" id="colors">
+        <option value="">--SVP, choisissez une couleur --</option>
+        <option value="${ colors }">${ colors }</option>
+    </select>
+    `
+})
+.catch(error => console.error(error))
