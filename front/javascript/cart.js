3bbc9f965c659d88858fab7a269bc661343c821c
VANG Robert <robert.vang@hotmail.fr>
2021-10-29 14:54:25 +0200

    deboggage avec ibrahima

--- front/javascript/cart.js
+++ front/javascript/cart.js
@@ -2,47 +2,60 @@
 let productsInLocalStorage = JSON.parse(localStorage.getItem('products'))
 console.log(productsInLocalStorage)
 const cartContent = document.getElementById('cart__items')
+const cartQuantity = document.getElementById("totalQuantity")
+const cartTotalPrice = document.getElementById("totalPrice")
 
 if (productsInLocalStorage === null || productsInLocalStorage == 0 ){
 const emptyCart = `<div>Le panier est vide</div>`
 cartContent.innerHTML = emptyCart
 } else {
-    for (product in productsInLocalStorage){
+    for (product of productsInLocalStorage){
         cartContent.innerHTML +=`
-        <article class="cart__item" data-id="${productsInLocalStorage.id}">
+        <article class="cart__item" data-id="${product._id}">
                 <div class="cart__item__img">
-                  <img src="${productsInLocalStorage[product].imageUrl}" alt="${productsInLocalStorage[product].altTxt}">
+                  <img src="${product.imageUrl}" alt="${product.altTxt}">
                 </div>
                 <div class="cart__item__content">
                   <div class="cart__item__content__titlePrice">
-                    <h2>${productsInLocalStorage[product].name}</h2>
-                    <p>${productsInLocalStorage[product].price/10}</p>
+                    <h2>${product.name}</h2>
+                    <p>${product.price/10}</p>
                   </div>
                   <div class="cart__item__content__settings">
                     <div class="cart__item__content__settings__quantity">
-                      <p>Qté : ${productsInLocalStorage[product].colors}</p>
-                      <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${productsInLocalStorage[product].quantity}">
+                      <p>Qté : ${product.colors}</p>
+                      <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${product.quantity}">
                     </div>
                     <div class="cart__item__content__settings__delete">
-                      <p class="deleteItem">Supprimer</p>
+                      <p class="deleteItem" id="${product._id}" data-colors="${product.colors}">Supprimer</p>
                     </div>
                   </div>
                 </div>
               </article>
         `
+    let totalQuantity = product.quantity
+    let totalPrice = product.price * product.quantity
+    cartQuantity.innerText = totalQuantity
+    cartTotalPrice.innerText = totalPrice /10
+
+    let btnDelete = document.getElementById(product._id)
+    document.addEventListener('click',function(e){
+    if(e.target && e.target.id== product._id && e.target.dataset.colors == product.colors){
+        //do something
+        console.log(product.colors)
+        }
+      });
     }
 }
-let btnDelete = document.getElementsByClassName('deleteItem') [0]
+/*
+btnDelete.addEventListener("click", function (e){
+    const id = e.target.id;
+    console.log(id)
+    return
+    const colors = e.target.dataset.colors
+    const productToDelete = productsInLocalStorage.find(element => element._id === id && element.colors === colors)
+    const remainingProducts = productsInLocalStorage.filter(element => element !== productToDelete)
 
-for (product in productsInLocalStorage){
-btnDelete.addEventListener("click", function (){
-    let idDelete = productsInLocalStorage[product].id
-    let colorDelete = productsInLocalStorage[product].colors
-
-    productsInLocalStorage = productsInLocalStorage.filter(element => element.id !== idDelete /*&& color => colors.colors !== colorDelete*/)
-
-    localStorage.setItem('products', JSON.stringify(productsInLocalStorage))
+    localStorage.setItem('products', JSON.stringify(remainingProducts))
     window.location.reload()
-    })
-    
-}
+})
+*/
