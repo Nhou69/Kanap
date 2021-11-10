42646e533ab1fd1fe0db7b6ffd642aa45b07038f
VANG Robert <robert.vang@hotmail.fr>
2021-11-10 10:18:11 +0100

    manque sur method post

--- front/javascript/cart.js
+++ front/javascript/cart.js
@@ -3,13 +3,13 @@ let productsInLocalStorage = JSON.parse(localStorage.getItem('products'))
 console.log(productsInLocalStorage)
 const cartContent = document.getElementById('cart__items')
 
-if (productsInLocalStorage === null || productsInLocalStorage == 0 ){
-const emptyCart = `<div>Le panier est vide</div>`
-cartContent.innerHTML = emptyCart
+if (productsInLocalStorage === null || productsInLocalStorage == 0) {
+    const emptyCart = `<div>Le panier est vide</div>`
+    cartContent.innerHTML = emptyCart
 } else {
-    for (product of productsInLocalStorage){
-        cartContent.innerHTML +=`
-        <article class="cart__item" data-id="${product._id}">
+    for (product of productsInLocalStorage) {
+        cartContent.innerHTML += `
+        <article class="cart__item" data-id="${product._id}" data-colors="${product.colors}">
                 <div class="cart__item__img">
                   <img src="${product.imageUrl}" alt="${product.altTxt}">
                 </div>
@@ -34,128 +34,145 @@ cartContent.innerHTML = emptyCart
 }
 
 /* suppression d'un article */
-for(let i = 0; i < productsInLocalStorage.length; i++){
-let btnDelete = document.getElementsByClassName('deleteItem') [i]
-btnDelete.addEventListener("click", function (e){
-  const id = e.target.id
-  const colors = e.target.dataset.colors
-  const productToDelete = productsInLocalStorage.find(element => element._id === id && element.colors == colors)
-  const remainingProducts = productsInLocalStorage.filter(element => element !== productToDelete)
+for (let i = 0; i < productsInLocalStorage.length; i++) {
+    let btnDelete = document.getElementsByClassName('deleteItem')[i]
+    btnDelete.addEventListener("click", function(e) {
+        const id = e.target.id
+        const colors = e.target.dataset.colors
+        const productToDelete = productsInLocalStorage.find(element => element._id === id && element.colors == colors)
+        const remainingProducts = productsInLocalStorage.filter(element => element !== productToDelete)
 
-  localStorage.setItem('products', JSON.stringify(remainingProducts))
-  window.location.reload()
-  })
+        localStorage.setItem('products', JSON.stringify(remainingProducts))
+        window.location.reload()
+    })
 }
 
 /* retirer ou ajouter un produit */
 
-for(let i = 0; i < productsInLocalStorage.length; i++){
-  let productQuantity = document.getElementsByClassName('itemQuantity') [i]
-  console.log(productQuantity.value)
-
-  /*productQuantity.value.addEventListener('change', function(e){
-      const foundProduct = productsInLocalStorage.find(element => element._id === selectedProduct._id)
-      const foundColor = productsInLocalStorage.find(colorValue => colorValue.colors === selectedProduct.colors)
-      if (foundProduct && foundColor) {
-          foundProduct.quantity += parseInt(quantityProducts.value)
-      } else {
-          productsInLocalStorage.push(selectedProduct)
-      }*/
-      //localStorage.setItem('products', JSON.stringify(productsInLocalStorage))
-
-      //window.location.reload()
+for (let i = 0; i < productsInLocalStorage.length; i++) {
+    let productQuantity = document.getElementsByClassName('itemQuantity')[i]
+    productQuantity.addEventListener('change', function(e) {
+        console.log(productQuantity.value);
+        const item = document.getElementsByClassName('cart__item')[i]
+        const productId = item.dataset.id;
+        const productColors = item.dataset.colors;
+        const foundProduct = productsInLocalStorage.find(element => element._id === productId && element.colors === productColors)
+        console.log(foundProduct);
+        foundProduct.quantity = productQuantity.value;
+        localStorage.setItem('products', JSON.stringify(productsInLocalStorage));
+        calculateTotalPriceProducts();
+    });
 }
 
-/* total quantité produit et total prix */
-const cartQuantity = document.getElementById("totalQuantity")
-const cartTotalPrice = document.getElementById("totalPrice")
-let totalQuantityProduct = 0
-let totalPriceProduct = 0
-for(product of productsInLocalStorage){
-  totalQuantityProduct = totalQuantityProduct + product.quantity
-  totalPriceProduct = totalPriceProduct + product.quantity * product.price
+
+function calculateTotalPriceProducts() {
+    /* total quantité produit et total prix */
+    const cartQuantity = document.getElementById("totalQuantity")
+    const cartTotalPrice = document.getElementById("totalPrice")
+    let totalQuantityProduct = 0
+    let totalPriceProduct = 0
+    for (product of productsInLocalStorage) {
+        totalQuantityProduct = parseInt(totalQuantityProduct) + parseInt(product.quantity);
+        totalPriceProduct = totalPriceProduct + product.quantity * product.price
+    }
+    cartQuantity.innerText = totalQuantityProduct
+    cartTotalPrice.innerText = totalPriceProduct / 10
 }
-cartQuantity.innerText = totalQuantityProduct
-cartTotalPrice.innerText = totalPriceProduct /10
+
+calculateTotalPriceProducts()
 
 /* Expression régulière du formulaire */
 /*Nom et Prénom */
-const form = document.getElementsByClassName('cart__order__form') [0]
-form.firstName.addEventListener('change', function(){
-  validName(this)
+const form = document.getElementsByClassName('cart__order__form')[0]
+form.firstName.addEventListener('change', function() {
+    validName(this)
 })
-form.lastName.addEventListener('change', function(){
-  validName(this)
+form.lastName.addEventListener('change', function() {
+    validName(this)
 })
 
-const validName = function (inputName) {
-  let nameRegExp = new RegExp("^[^- ][a-zA-Z '\-àâäéèêëïîôöùûü]*[^- ]$", "g")
-  let testName = nameRegExp.test(inputName.value);
-  if (testName) {
-    inputName.nextElementSibling.innerHTML = "Validé"
-    return true
-  } else {
-    inputName.nextElementSibling.innerHTML = "Saisissez votre prénom ou votre nom"
-    return false
-  }
+const validName = function(inputName) {
+    let nameRegExp = new RegExp("^[^- ][a-zA-Z '\-àâäéèêëïîôöùûü]*[^- ]$", "g")
+    let testName = nameRegExp.test(inputName.value);
+    if (testName) {
+        inputName.nextElementSibling.innerHTML = "Validé"
+        return true
+    } else {
+        inputName.nextElementSibling.innerHTML = "Saisissez votre prénom ou votre nom"
+        return false
+    }
 }
 
 /*adresse */
-form.address.addEventListener('change', function(){
-  validAddress(this)
+form.address.addEventListener('change', function() {
+    validAddress(this)
 })
 
-const validAddress = function (inputAdress) {
-  let addressRegExp = new RegExp("^[0-9]{1,4} [^- ][a-zA-Z '\-àâäéèêëïîôöùûü]*[^- ]$", "g")
-  let testAdress = addressRegExp.test(inputAdress.value)
-  if (testAdress) {
-    inputAdress.nextElementSibling.innerHTML = "Validé"
-    return true
-  } else {
-    inputAdress.nextElementSibling.innerHTML = "Saisissez votre adresse"
-    return false
-  }
+const validAddress = function(inputAdress) {
+    let addressRegExp = new RegExp("^[0-9]{1,4} [^- ][a-zA-Z '\-àâäéèêëïîôöùûü]*[^- ]$", "g")
+    let testAdress = addressRegExp.test(inputAdress.value)
+    if (testAdress) {
+        inputAdress.nextElementSibling.innerHTML = "Validé"
+        return true
+    } else {
+        inputAdress.nextElementSibling.innerHTML = "Saisissez votre adresse"
+        return false
+    }
 }
 
 /*Ville*/
-form.city.addEventListener('change', function(){
-  validCity(this)
+form.city.addEventListener('change', function() {
+    validCity(this)
 })
 
-const validCity = function (inputCity) {
-  let cityRegExp = new RegExp("^[^- ][a-zA-Z '\-àâäéèêëïîôöùûü]*[^- ]$", "g")
-  let testCity = cityRegExp.test(inputCity.value)
-  if (testCity) {
-    inputCity.nextElementSibling.innerHTML = "Validé"
-    return true
-  } else {
-    inputCity.nextElementSibling.innerHTML = "Saisissez votre ville"
-    return false
-  }
+const validCity = function(inputCity) {
+    let cityRegExp = new RegExp("^[^- ][a-zA-Z '\-àâäéèêëïîôöùûü]*[^- ]$", "g")
+    let testCity = cityRegExp.test(inputCity.value)
+    if (testCity) {
+        inputCity.nextElementSibling.innerHTML = "Validé"
+        return true
+    } else {
+        inputCity.nextElementSibling.innerHTML = "Saisissez votre ville"
+        return false
+    }
 }
 
 /*Email*/
-form.email.addEventListener("change", function () {
-  validEmail(this);
+form.email.addEventListener("change", function() {
+    validEmail(this);
 })
 
-const validEmail = function (inputEmail) {
-  let emailRegExp = new RegExp("^[a-zA-Z0-9.-_]+[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,10}$","g")
-  let testEmail = emailRegExp.test(inputEmail.value)
-  if (testEmail) {
-    inputEmail.nextElementSibling.innerHTML = "Validé"
-    return true
-  } else {
-    inputEmail.nextElementSibling.innerHTML = "Saisissez votre adresse mail complète"
-    return false
-  }
+const validEmail = function(inputEmail) {
+    let emailRegExp = new RegExp("^[a-zA-Z0-9.-_]+[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,10}$", "g")
+    let testEmail = emailRegExp.test(inputEmail.value)
+    if (testEmail) {
+        inputEmail.nextElementSibling.innerHTML = "Validé"
+        return true
+    } else {
+        inputEmail.nextElementSibling.innerHTML = "Saisissez votre adresse mail complète"
+        return false
+    }
 }
 
 /* envoi du formulaire */
-form.addEventListener('submit', function(e){
-  if (validName(form.firstName) && validName(form.lastName) && validAddress(form.address) && validCity(form.city) && validEmail(form.email)){
-    form.submit()
-  } else{
-    e.preventDefault()
-  }
+form.addEventListener('submit', function(e) {
+    if (validName(form.firstName) && validName(form.lastName) && validAddress(form.address) && validCity(form.city) && validEmail(form.email)) {
+        form.submit()
+    } else {
+        e.preventDefault()
+    }
 })
+
+function makeOrder() {
+    {
+        contact: {
+            firstName: 'ibrahima',
+            lastName: 'ciss',
+            address: 'House 71 - Cherif Macky Sall City - Guediawaye - Dakar',
+            city: 'dakar',
+            email: 'bionik6@gmail.com'
+        }
+        products: ['43kjlfjdl3l2kjfllsd', '21oikjfldkslajkl', '32lkjlfkjslkjfsd']
+    }
+
+}
