b1e3606343125994800fa49fd4b7360a6f0dd1ee
VANG Robert <robert.vang@hotmail.fr>
2021-11-11 16:03:11 +0100

    en cours de résolution du bug aléatoire

--- front/javascript/cart.js
+++ front/javascript/cart.js
@@ -31,174 +31,178 @@ if (productsInLocalStorage === null || productsInLocalStorage == 0) {
               </article>
         `
     }
-}
-/* suppression d'un article */
-for (let i = 0; i < productsInLocalStorage.length; i++) {
-    let btnDelete = document.getElementsByClassName('deleteItem')[i]
-    btnDelete.addEventListener("click", function(e) {
-        const id = e.target.id
-        const colors = e.target.dataset.colors
-        const productToDelete = productsInLocalStorage.find(element => element._id === id && element.colors == colors)
-        const remainingProducts = productsInLocalStorage.filter(element => element !== productToDelete)
+        
+    /* suppression d'un article */
+    for (let i = 0; i < productsInLocalStorage.length; i++) {
+        let btnDelete = document.getElementsByClassName('deleteItem')[i]
+        btnDelete.addEventListener("click", function(e) {
+            const id = e.target.id
+            const colors = e.target.dataset.colors
+            const productToDelete = productsInLocalStorage.find(element => element._id === id && element.colors == colors)
+            const remainingProducts = productsInLocalStorage.filter(element => element !== productToDelete)
 
-        localStorage.setItem('products', JSON.stringify(remainingProducts))
-        window.location.reload()
-        alert('Vous avez supprimé : ' + product.quantity + 'x ' + product.name + ' ' + product.colors)
-    })
-}
-
-/* retirer ou ajouter un produit */
-
-for (let i = 0; i < productsInLocalStorage.length; i++) {
-    let productQuantity = document.getElementsByClassName('itemQuantity')[i]
-    productQuantity.addEventListener('change', function(e) {
-        console.log(productQuantity.value);
-        const item = document.getElementsByClassName('cart__item')[i]
-        const productId = item.dataset.id;
-        const productColors = item.dataset.colors;
-        const foundProduct = productsInLocalStorage.find(element => element._id === productId && element.colors === productColors)
-        foundProduct.quantity = productQuantity.value;
-        localStorage.setItem('products', JSON.stringify(productsInLocalStorage));
-        calculateTotalPriceProducts();
-    });
-}
-
-
-function calculateTotalPriceProducts() {
-    /* total quantité produit et total prix */
-    const cartQuantity = document.getElementById("totalQuantity")
-    const cartTotalPrice = document.getElementById("totalPrice")
-    let totalQuantityProduct = 0
-    let totalPriceProduct = 0
-    for (product of productsInLocalStorage) {
-        totalQuantityProduct = parseInt(totalQuantityProduct) + parseInt(product.quantity);
-        totalPriceProduct = totalPriceProduct + product.quantity * product.price
-    }
-    cartQuantity.innerText = totalQuantityProduct
-    cartTotalPrice.innerText = totalPriceProduct / 10
-}
-
-calculateTotalPriceProducts()
-
-/* Expression régulière du formulaire */
-/*Nom et Prénom */
-const form = document.getElementsByClassName('cart__order__form')[0]
-
-form.firstName.addEventListener('change', function() {
-    validName(this)
-})
-form.lastName.addEventListener('change', function() {
-    validName(this)
-})
-
-const validName = function(inputName) {
-    let nameRegExp = new RegExp("^[^- ][a-zA-Z '\-àâäéèêëïîôöùûü]*[^- ]$", "g")
-    let testName = nameRegExp.test(inputName.value);
-    if (testName) {
-        inputName.nextElementSibling.innerHTML = "Validé"
-        inputName.nextElementSibling.style.color = "green"
-        return true
-    } else {
-        inputName.nextElementSibling.innerHTML = "Saisissez votre prénom ou votre nom"
-        inputName.nextElementSibling.style.color = "red"
-        return false
-    }
-}
-
-/*adresse */
-form.address.addEventListener('change', function() {
-    validAddress(this)
-})
-
-const validAddress = function(inputAdress) {
-    let addressRegExp = new RegExp("^[0-9]{1,4} [^- ][a-zA-Z '\-àâäéèêëïîôöùûü]*[^- ]$", "g")
-    let testAdress = addressRegExp.test(inputAdress.value)
-    if (testAdress) {
-        inputAdress.nextElementSibling.innerHTML = "Validé"
-        inputAdress.nextElementSibling.style.color = "green"
-        return true
-    } else {
-        inputAdress.nextElementSibling.innerHTML = "Saisissez votre adresse"
-        inputAdress.nextElementSibling.style.color = "red"
-        return false
-    }
-}
-
-/*Ville*/
-form.city.addEventListener('change', function() {
-    validCity(this)
-})
-
-const validCity = function(inputCity) {
-    let cityRegExp = new RegExp("^[^- ][a-zA-Z '\-àâäéèêëïîôöùûü]*[^- ]$", "g")
-    let testCity = cityRegExp.test(inputCity.value)
-    if (testCity) {
-        inputCity.nextElementSibling.innerHTML = "Validé"
-        inputCity.nextElementSibling.style.color = "green"
-        return true
-    } else {
-        inputCity.nextElementSibling.innerHTML = "Saisissez votre ville"
-        inputCity.nextElementSibling.style.color = "red"
-        return false
-    }
-}
-
-/*Email*/
-form.email.addEventListener("change", function() {
-    validEmail(this);
-})
-
-const validEmail = function(inputEmail) {
-    let emailRegExp = new RegExp("^[a-zA-Z0-9.-_]+[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,10}$", "g")
-    let testEmail = emailRegExp.test(inputEmail.value)
-    if (testEmail) {
-        inputEmail.nextElementSibling.innerHTML = "Validé"
-        inputEmail.nextElementSibling.style.color = "green"
-        return true
-    } else {
-        inputEmail.nextElementSibling.innerHTML = "Saisissez votre adresse mail complète"
-        inputEmail.nextElementSibling.style.color = "red"
-        return false
-    }
-}
-
-/* envoi du formulaire */
-form.addEventListener('submit', function(e) {
-    if (validName(form.firstName) && validName(form.lastName) && validAddress(form.address) && validCity(form.city) && validEmail(form.email)) {
-        form.submit()
-        makeOrder()
-    } else {
-        e.preventDefault()
-        alert("Veuillez remplir le formulaire correctement s'il vous plait !");
-    }
-})
-
-const sendOrderToBackEnd = (theOrder) => {
-    fetch("http://localhost:3000/api/products/order", {
-            method: "POST",
-            headers: {
-                Accept: "application/json",
-                "Content-type": "application/JSON",
-            },
-            body: JSON.stringify(theOrder),
+            localStorage.setItem('products', JSON.stringify(remainingProducts))
+            window.location.reload()
+            alert('Vous avez supprimé : ' + product.quantity + 'x ' + product.name + ' ' + product.colors)
         })
-        .then((data) => data.json())
-        .then((data) => {
-            console.log(data);
-            console.log(data.orderId);
-            const orderId = data.orderId;
-            localStorage.setItem("orderId", orderId);
+    }
 
-            //Envoi de l'utilisateur vers la page de confirmation en supprimant le localStorage
-            window.location.href = "confirmation.html" + "?" + "name" + "=" + orderId;
-            /* localStorage.removeItem('products');
-            localStorage.removeItem('orderId'); */
-            ['products', 'orderId'].forEach(item => localStorage.removeItem(item));
+    /* retirer ou ajouter un produit */
+    for (let i = 0; i < productsInLocalStorage.length; i++) {
+        let productQuantity = document.getElementsByClassName('itemQuantity')[i]
+        productQuantity.addEventListener('change', function(e) {
+            console.log(productQuantity.value);
+            const item = document.getElementsByClassName('cart__item')[i]
+            const productId = item.dataset.id;
+            const productColors = item.dataset.colors;
+            const foundProduct = productsInLocalStorage.find(element => element._id === productId && element.colors === productColors)
+            foundProduct.quantity = productQuantity.value;
+            localStorage.setItem('products', JSON.stringify(productsInLocalStorage));
+            calculateTotalPriceProducts();
         });
-};
+    }
 
-function makeOrder() {
-    {
+
+    function calculateTotalPriceProducts() {
+        /* total quantité produit et total prix */
+        const cartQuantity = document.getElementById("totalQuantity")
+        const cartTotalPrice = document.getElementById("totalPrice")
+        let totalQuantityProduct = 0
+        let totalPriceProduct = 0
+        for (product of productsInLocalStorage) {
+            totalQuantityProduct = parseInt(totalQuantityProduct) + parseInt(product.quantity);
+            totalPriceProduct = totalPriceProduct + product.quantity * product.price
+        }
+        cartQuantity.innerText = totalQuantityProduct
+        cartTotalPrice.innerText = totalPriceProduct / 10
+    }
+
+    calculateTotalPriceProducts()
+
+    /* Expression régulière du formulaire */
+    /*Nom et Prénom */
+    const form = document.getElementsByClassName('cart__order__form')[0]
+
+    form.firstName.addEventListener('change', function() {
+        validName(this)
+    })
+    form.lastName.addEventListener('change', function() {
+        validName(this)
+    })
+
+    const validName = function(inputName) {
+        let nameRegExp = new RegExp("^[^- ][a-zA-Z '\-àâäéèêëïîôöùûü]*[^- ]$", "g")
+        let testName = nameRegExp.test(inputName.value);
+        if (testName) {
+            inputName.nextElementSibling.innerHTML = "Validé"
+            inputName.nextElementSibling.style.color = "green"
+            return true
+        } else {
+            inputName.nextElementSibling.innerHTML = "Saisissez votre prénom ou votre nom"
+            inputName.nextElementSibling.style.color = "red"
+            return false
+        }
+    }
+
+    /*adresse */
+    form.address.addEventListener('change', function() {
+        validAddress(this)
+    })
+
+    const validAddress = function(inputAdress) {
+        let addressRegExp = new RegExp("^[0-9]{1,4} [^- ][a-zA-Z '\-àâäéèêëïîôöùûü]*[^- ]$", "g")
+        let testAdress = addressRegExp.test(inputAdress.value)
+        if (testAdress) {
+            inputAdress.nextElementSibling.innerHTML = "Validé"
+            inputAdress.nextElementSibling.style.color = "green"
+            return true
+        } else {
+            inputAdress.nextElementSibling.innerHTML = "Saisissez votre adresse"
+            inputAdress.nextElementSibling.style.color = "red"
+            return false
+        }
+    }
+
+    /*Ville*/
+    form.city.addEventListener('change', function() {
+        validCity(this)
+    })
+
+    const validCity = function(inputCity) {
+        let cityRegExp = new RegExp("^[^- ][a-zA-Z '\-àâäéèêëïîôöùûü]*[^- ]$", "g")
+        let testCity = cityRegExp.test(inputCity.value)
+        if (testCity) {
+            inputCity.nextElementSibling.innerHTML = "Validé"
+            inputCity.nextElementSibling.style.color = "green"
+            return true
+        } else {
+            inputCity.nextElementSibling.innerHTML = "Saisissez votre ville"
+            inputCity.nextElementSibling.style.color = "red"
+            return false
+        }
+    }
+
+    /*Email*/
+    form.email.addEventListener("change", function() {
+        validEmail(this);
+    })
+
+    const validEmail = function(inputEmail) {
+        let emailRegExp = new RegExp("^[a-zA-Z0-9.-_]+[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,10}$", "g")
+        let testEmail = emailRegExp.test(inputEmail.value)
+        if (testEmail) {
+            inputEmail.nextElementSibling.innerHTML = "Validé"
+            inputEmail.nextElementSibling.style.color = "green"
+            return true
+        } else {
+            inputEmail.nextElementSibling.innerHTML = "Saisissez votre adresse mail complète"
+            inputEmail.nextElementSibling.style.color = "red"
+            return false
+        }
+    }
+
+    /* envoi du formulaire */
+    form.addEventListener('submit', function(e) {
+        if (validName(form.firstName) && validName(form.lastName) && validAddress(form.address) && validCity(form.city) && validEmail(form.email)) {
+            //form.submit()
+            makeOrder()
+        } else {
+            alert("Veuillez remplir le formulaire correctement s'il vous plait !");
+        }
+    })
+
+    const sendOrderToBackEnd = (theOrder) => {
+        fetch("http://localhost:3000/api/products/order", {
+                method: "POST",
+                headers: {
+                    Accept: "application/json",
+                    "Content-type": "application/JSON",
+                },
+                body: JSON.stringify(theOrder),
+            })
+            .then((data) => data.json())
+            .then((data) => {
+                console.log(data);
+                console.log(data.orderId);
+                const orderId = data.orderId;
+                localStorage.setItem("orderId", orderId);
+
+                //Envoi de l'utilisateur vers la page de confirmation en supprimant le localStorage
+                window.location.href = "confirmation.html" + "?" + "name" + "=" + orderId;
+                /* Codes permettant de supprimer le localStorage 
+                localStorage.removeItem('products');
+                localStorage.removeItem('orderId'); équivalent ci dessous*/
+                ['products', 'orderId'].forEach(item => localStorage.removeItem(item));
+                //localStorage.clear()
+            });
+    };
+
+    function makeOrder() {
+        const products = []
+        for (product of productsInLocalStorage) {
+            let productId = product._id
+            products.push(productId)
+        }
         const contact = {
             firstName: form.firstName.value,
             lastName: form.lastName.value,
@@ -206,16 +210,13 @@ function makeOrder() {
             city: form.city.value,
             email: form.email.value,
         }
-        const products = []
-        for (product of productsInLocalStorage) {
-            const productId = product._id
-            products.push(productId)
-        }
+            
         const theOrder = {
                 contact,
                 products
             }
-            // localStorage.setItem("products", JSON.stringify(theOrder))
-        sendOrderToBackEnd(theOrder)
+
+            //localStorage.setItem("products", JSON.stringify(theOrder))
+            sendOrderToBackEnd(theOrder)
     }
 }
